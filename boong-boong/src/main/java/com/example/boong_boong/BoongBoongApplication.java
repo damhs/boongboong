c5e8422a394964090b6695b053705870db3d78de
damhs <ihansaem1@kaist.ac.kr>
2025-01-10 09:13:14 +0900

    BE basic

---
+++ boong-boong/src/main/java/com/example/boong_boong/BoongBoongApplication.java
@@ -0,0 +1,13 @@
+package com.example.boong_boong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoongBoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoongBoongApplication.class, args);
+	}
+
+}
---
+++ boong-boong/src/test/java/com/example/boong_boong/BoongBoongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boong_boong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoongBoongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
