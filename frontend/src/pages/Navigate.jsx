69c35813e2335f34a6b69990d1c2042f4f4237c3
Jimin Heo <zimin322@gmail.com>
2025-01-15 14:00:12 +0900

    FEAT: add navigate page

--- frontend/src/pages/Navigate.jsx
+++ frontend/src/pages/Navigate.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import MapEdit from '../components/MapEdit';
+import styles from "./Home.module.css"
+import { ReactComponent as NavigationIcon } from "../assets/icons/navigation_icon.svg";
+
 
 function Navigate() {
   return (
-    <div>
-      <h1>Navigation</h1>
+    <div style={{ textAlign: 'center' }}>
+      <div style={{ position: 'relative', height: '80vh' }}> {/* 부모 요소에 상대 위치 설정 */}
+        <MapEdit />
+        <span>status</span>
+        <span>speed</span>
+      </div>
+      {/* <TrafficInfo /> */}
     </div>
   );
 }
